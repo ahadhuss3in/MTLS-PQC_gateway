8ec1f3d63a0aa20888ea7c8a87f501635305a394
ahadhuss3in <ahadasif.cs22@sahyadri.edu.in>
2025-11-21 12:19:05 +0530

    Added MTLS-fz project into main repository

---
+++ MTLS-fz/src/main/java/com/example/mtlsfz/MtlsFzApplication.java
@@ -0,0 +1,13 @@
+package com.example.mtlsfz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtlsFzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtlsFzApplication.class, args);
+    }
+
+}
